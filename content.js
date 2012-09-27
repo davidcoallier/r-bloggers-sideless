30ffdffcc32f217d22afb7c7d99d68b10996bdd3
David Coallier <dcoallier@engineyard.com>
2012-09-27 14:05:39 +0100

    - Added new handler for the main page which makes it look realllll   sweet. - Added a release folder with the packed extension.

--- content.js
+++ content.js
@@ -1,4 +1,10 @@
+
 $('#secondsidebar').remove();
 $('#sidebar').remove();
 var w = $('#mainwrapper').width();
 $('#leftcontent').css({width: w + "px"})
+
+twocol = $('#twocol')
+if (twocol != undefined) {
+    $('#twocol').width(w+'px');
+}
